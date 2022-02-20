993b1593f8ad366eb1dbfec88897a6436ccab91a
nipengg <nevillecorneliustj@gmail.com>
2022-02-21 00:48:57 +0700

    Create about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,16 +9,35 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+                    <div class="mt-8 text-2xl" style="font-size: 18pt">
+                        Vision
+                    </div>
+                    <br>
+                    <hr>
                     <div class="mt-8 text-2xl">
-                        About page
+                        Empowering everyone to open new doors
+                        through programming
                     </div>
 
                     <div class="mt-6 text-gray-500">
-                        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-                        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-                        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-                        ecosystem to be a breath of fresh air. We hope you love it.
+                        We believe programming is a power that can change ourselves, our surroundings, and the world, regardless of age, gender, race, or position. We want everyone to have this wonderful power. And we want to open new doors of many lives alone.
                     </div>
+
+                    <div class="mt-8 text-2xl" style="font-size: 18pt">
+                        Mission
+                    </div>
+                    <br>
+                    <hr>
+
+                    <div class="mt-8 text-2xl">
+                        Be the gate to the exciting world of programming.
+                        Be the path to an independent coder.
+                    </div>
+
+                    <div class="mt-6 text-gray-500">
+                        Learning to code gave us the power to create. It gave us confidence and opened new doors. We want to make this power accessible to anyone, and open up the possibilities for everyone.
+                    </div>
+                    <br>
                 </div>
             </div>
         </div>
